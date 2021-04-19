9304bceba2712324081f2ea2d24a8d3f3b9a3198
Alexander Oro <oroalexander004@gmail.com>
2021-04-19 20:10:27 +0900

    add: Establish DB connection to Atlas

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config( {path:'.env'} );
+// require('dotenv').config(); ?? not working
 
 const app = express();
 
 app.use(cors());
+
+const connectDB = async () => {
+    try{
+        // mongo connection string
+        const con = await mongoose.connect(process.env.DB_CONNECTION_STRING, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        })
+
+        console.log(`Database connection established || ${con.connection.host}`);
+    } catch(err) {
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+connectDB();
+
+
 const port = process.env.PORT || 3000;
 
 
@@ -20,3 +45,7 @@ app.get('/',(req,res) => {
 app.listen(port, ()=> {
     console.log(`Application server is running at http://localhost:${port}`);
 });
+
+//cbs_db_admin
+//V2uPHX1kFfexCGUB
+
